f53ea4f7c6c39d16b74b99482900aa834351547c
liar780@gmail.com <liar780@gmail.com>
2022-12-26 19:58:41 +0700

    save tabel

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import UserList from "./component/UserList.jsx";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import AddUser from "./component/AddUser.jsx";
 
 function App() {
 
 	return (
-		<div className='App'>
-			<UserList/>
-		</div>
+		<BrowserRouter>
+			<Routes>
+				<Route path={'/'} element={<UserList/>}/>
+				<Route path={'/add'} element={<AddUser/>}/>
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
