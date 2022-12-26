e0a311bd738d892c6f93a8a0ec7222a51e87240e
liar780@gmail.com <liar780@gmail.com>
2022-12-26 14:07:27 +0700

    backend

---
+++ backend/controller/UserController.js
@@ -0,0 +1,13 @@
+// const Sequelize = require('sequelize');
+const User = require("../models/UserModel");
+
+const getUsers = async (req, res) => {
+	try {
+		const response = await User.findAll();
+		res.status(200).json(response);
+	} catch (error) {
+		console.log(error.message);
+	}
+};
+
+module.exports = {getUsers}
---
+++ frontEnd/src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+import reactLogo from './assets/react.svg';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      hello
+    </div>
+  );
+}
+
+export default App;
